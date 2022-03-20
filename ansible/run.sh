3408bb21a8682d9ac2ea18272ef5e97a66614d17
gorinskiy <gorinskiy@magora-systems.com>
2022-03-21 00:44:03 +0700

    debian more

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 SCRIPT=$(readlink -f "$0")
-PATH=$(dirname "$SCRIPT")
+ANS_PATH=$(dirname "$SCRIPT")
 
 ansible-playbook -i inventory/hosts-develop.yml full_install.yml -l develop
