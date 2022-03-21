2ad353f695d6f43aaec76abd61aa29b9923eaa02
viktor-gorinskiy <viktor@gorinskiy.ru>
2022-03-21 08:53:16 +0700

    all_run

--- ansible/run.sh
+++ ansible/run.sh
@@ -2,4 +2,4 @@
 SCRIPT=$(readlink -f "$0")
 ANS_PATH=$(dirname "$SCRIPT")
 
-ansible-playbook -i inventory/hosts-develop.yml full_install.yml -l develop
+ansible-playbook -i inventory/hosts-develop.yml full_install.yml -l main
